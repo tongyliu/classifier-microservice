4ecc7037c59ccea936364cfb3024c3b10fc3e686
Tong Liu <tongyliu@gmail.com>
2022-10-01 11:43:38 -0700

    Set up env and MySQL connection

--- api/application.py
+++ api/application.py
@@ -1,24 +1,12 @@
-"""
-TODO. For instance:
-
-1. using flask:
-
+import db
 from flask import Flask
+
 application = Flask(__name__)
 
 
-2. using django:
+@application.route('/health', methods=['GET'])
+def health():
+    conn = db.connect()
+    print(conn)
+    return {'message': 'Ok'}
 
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your.django.settings")
-application = get_wsgi_application()
-
-
-3. using raw WSGI (https://www.python.org/dev/peps/pep-3333/#the-application-framework-side):
-
-def application(environ, start_response):
-    start_response('200 OK', [('Content-type', 'application/json')])
-    return ['{"message": "ok"}']
-"""
-
-application = None
--- api/db.py
+++ api/db.py
@@ -6,5 +6,5 @@ def connect():
     """ https://mysqlclient.readthedocs.io/user_guide.html#mysqldb """
     return MySQLdb.connect(host=os.environ['MYSQL_HOST'],
                            user=os.environ['MYSQL_USER'],
-                           passwd=os.environ['MYSQL_ROOT_PASSWORD'],
+                           passwd=os.environ['MYSQL_PASSWORD'],
                            db=os.environ['MYSQL_DATABASE'])
